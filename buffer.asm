05c33b45e8ac5a24247e941c22035f6ecef75510
mohammad-ghaderi <mohammadunique2002@gmail.com>
2025-11-19 23:21:55 +0330

    memory layout

---
+++ buffer.asm
@@ -0,0 +1,34 @@
+global conv1_w, conv2_w, conv3_w
+global conv1_b, conv2_b, conv3_b
+global input, conv1_out, pool1_out, conv2_out
+global pool2_out, conv3_out, pool3_out, fc1_out
+global output, fc1_w, fc1_b
+
+B equ 32           
+
+section .bss
+
+conv1_w resd 3*3*3*32
+conv2_w resd 3*3*3*32
+conv3_w resd 3*3*32*64
+
+conv1_b resd 32
+conv2_b resd 64
+conv3_b resd 128
+
+input resd B*3*128*128     
+conv1_out resd B*32*128*128
+pool1_out resd B*32*64*64  
+
+conv2_out resd B*64*64*64  
+pool2_out resd B*64*32*32  
+
+conv3_out resd B*128*32*32 
+pool3_out resd B*128*16*16 
+
+fc1_out resd B*128         
+output resd  B
+
+fc1_w resd 32767*128         
+fc1_b resd 128         
+
