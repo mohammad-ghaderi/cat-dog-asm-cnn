0c675333c34d4a5f613e4e798f866fdfbf63a159
mohammad-ghaderi <mohammadunique2002@gmail.com>
2025-11-20 14:49:19 +0330

    fix bugs in layout sizes and names

--- buffer.asm
+++ buffer.asm
@@ -1,6 +1,7 @@
 global conv1_w, conv2_w, conv3_w
 global conv1_b, conv2_b, conv3_b
-global input, conv1_out, pool1_out, conv2_out
+global input, label
+global conv1_out, pool1_out, conv2_out
 global pool2_out, conv3_out, pool3_out, fc1_out
 global output, fc1_w, fc1_b
 global B
@@ -11,14 +12,14 @@ section .bss
 
 conv1_w resd 3*3*3*32
 conv2_w resd 3*3*3*32
-conv3_w resd 3*3*32*64
+conv3_w resd 3*3*64*128
 
 conv1_b resd 32
 conv2_b resd 64
 conv3_b resd 128
 
 input resd B*3*128*128  
-labels resb B
+label resb B
 
 conv1_out resd B*32*128*128
 pool1_out resd B*32*64*64  
@@ -32,6 +33,6 @@ pool3_out resd B*128*16*16
 fc1_out resd B*128         
 output resd  B
 
-fc1_w resd 32767*128         
+fc1_w resd 128*16*16*128         
 fc1_b resd 128         
 
