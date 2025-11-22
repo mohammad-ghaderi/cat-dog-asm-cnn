e4e5ec05d59d60164611605cd302f30e69e90883
mohammad-ghaderi <mohammadunique2002@gmail.com>
2025-11-22 13:20:29 +0330

    fix the mistake in conv dimention

--- buffer.asm
+++ buffer.asm
@@ -3,7 +3,7 @@ global conv1_b, conv2_b, conv3_b
 global input, label
 global conv1_out, pool1_out, conv2_out
 global pool2_out, conv3_out, pool3_out, fc1_out
-global output, fc1_w, fc1_b
+global output, fc1_w, fc1_b, fc2_w, fc2_b
 global B
 
 B equ 32        ; Batch size     
@@ -11,7 +11,7 @@ B equ 32        ; Batch size
 section .bss
 
 conv1_w resd 3*3*3*32
-conv2_w resd 3*3*3*32
+conv2_w resd 3*3*32*64
 conv3_w resd 3*3*64*128
 
 conv1_b resd 32
