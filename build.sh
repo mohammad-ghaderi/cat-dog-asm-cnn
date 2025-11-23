1ae20e14a6621ea3076d31141ae8b378a85f87ce
mohammad-ghaderi <mohammadunique2002@gmail.com>
2025-11-23 18:50:16 +0330

    build with padding asm

--- build.sh
+++ build.sh
@@ -7,9 +7,10 @@ nasm -f elf64 -g -F dwarf _start.asm -o obj/_start.o
 nasm -f elf64 -g -F dwarf loader.asm -o obj/loader.o
 nasm -f elf64 -g -F dwarf buffer.asm -o obj/buffer.o
 nasm -f elf64 -g -F dwarf parameters.asm -o obj/parameters.o
+nasm -f elf64 -g -F dwarf padding.asm -o obj/padding.o
 
 # Link
-ld -o model obj/parameters.o obj/buffer.o obj/loader.o  obj/_start.o \
+ld -o model obj/padding.o obj/parameters.o obj/buffer.o obj/loader.o  obj/_start.o \
    -dynamic-linker /lib64/ld-linux-x86-64.so.2 -lc -lm
 
 
