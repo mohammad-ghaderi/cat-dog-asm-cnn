905e4cc669bb749913657b84fdf7f95b5d9e676a
mohammad-ghaderi <mohammadunique2002@gmail.com>
2025-11-24 22:42:37 +0330

    fix bug in order of linking .obj files

--- build.sh
+++ build.sh
@@ -8,9 +8,10 @@ nasm -f elf64 -g -F dwarf loader.asm -o obj/loader.o
 nasm -f elf64 -g -F dwarf buffer.asm -o obj/buffer.o
 nasm -f elf64 -g -F dwarf parameters.asm -o obj/parameters.o
 nasm -f elf64 -g -F dwarf padding.asm -o obj/padding.o
+nasm -f elf64 -g -F dwarf debug_tools.asm -o obj/debug.o
 
 # Link
-ld -o model obj/padding.o obj/parameters.o obj/buffer.o obj/loader.o  obj/_start.o \
+ld -o model obj/buffer.o obj/debug.o obj/padding.o obj/parameters.o obj/loader.o  obj/_start.o \
    -dynamic-linker /lib64/ld-linux-x86-64.so.2 -lc -lm
 
 
