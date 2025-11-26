b453b7cee1dfcbc456c749706fac2e5ab0d4e442
mohammad-ghaderi <mohammadunique2002@gmail.com>
2025-11-26 10:26:25 +0330

    change conv to first iterate on i,j then filter, (only one sample), using SIMD AVX-512 to parallelism 16 numbers once, and also using 19 of zmm to reduce read and write from RAM

--- convolution.asm
+++ convolution.asm
@@ -1,4 +1,4 @@
-global conv1_3x3
+global conv2d
 
 extern input, conv1_out
 extern conv1_w, conv1_b
@@ -6,87 +6,62 @@ extern B
 
 section .text
 
+; rdi = filter address
 ; rsi = x adress
 ; rdx = output address
 ; rcx for F loop
-; r8 for B loop
-; r9 for R loop
-; r10 for C loop
+; r8 for R loop
+; r9 for C loop
 ; zmm0 for output
-conv1_3x3:
-    mov rbx, 0000000111111111b
-    kmovw k1, ebx               ; mask
-    mov rbx, 3*130*4            ; one layer below in 3d input
+conv2d:
+    xor r8, r8      ; i
+    xor r9, r9      ; j
+    xor rcx, rcx        ; filter idx
+    vxorps zmm0, zmm0, zmm0         ; answer of zmm(4,5,6)
+    vxorps zmm7, zmm7, zmm7         ; answer of zmm(8,9,10)
+    vxorps zmm11, zmm11, zmm11      ; answer of zmm(12,13,14)
+    vxorps zmm15, zmm15, zmm15      ; answer of zmm(16,17,18)
 
-    vxorps zmm0, zmm0, zmm0
+
+    ; r10, r11, k1, bias ---------
+.i_loop:
+.j_loop:
+    vmovdqu32 zmm1 {k1}{z}, [rsi]
+    vmovdqu32 zmm2 {k1}{z}, [rsi + r10]
+    vmovdqu32 zmm3 {k1}{z}, [rsi + r11]
     
-    vmovdqu32 zmm2 {k1}{z}, [rdi]
-    vmovdqu32 zmm3 {k1}{z}, [rdi+9*4]
-    vmovdqu32 zmm4 {k1}{z}, [rdi+18*4]
-
-    lea rsi, [rel input]    ; address of input
-    lea rdx, [rel conv1_out]; address of ouput
-
-    xor rcx, rcx        ; filter index
 .filter_loop:
+    ; each time calculate for 4 filter, to reduce read and write current answer of filters on RAM by using registers
+    ; 1st filter of 4
+    vmovdqu32 zmm4 {k1}{z}, [rdi]               ; from layer 1
+    vmovdqu32 zmm5 {k1}{z}, [rdi + r12]         ; from layer 2
+    vmovdqu32 zmm6 {k1}{z}, [rdi + r12*2]       ; from layer 3
+    vfmadd231ps zmm0, zmm1, zmm4                ; zmm0 += zmm1 * zmm4
+    vfmadd231ps zmm0, zmm2, zmm5                ; zmm0 += zmm2 * zmm5
+    vfmadd231ps zmm0, zmm3, zmm6                ; zmm0 += zmm3 * zmm6
+    ; 2nd filter of 4
+    vmovdqu32 zmm8 {k1}{z}, [rdi]               ; from layer 1
+    vmovdqu32 zmm9 {k1}{z}, [rdi + r12]         ; from layer 2
+    vmovdqu32 zmm10 {k1}{z}, [rdi + r12*2]      ; from layer 3
+    vfmadd231ps zmm7, zmm1, zmm8                ; zmm7 += zmm1 * zmm8
+    vfmadd231ps zmm7, zmm2, zmm9                ; zmm7 += zmm2 * zmm9
+    vfmadd231ps zmm7, zmm3, zmm10               ; zmm7 += zmm3 * zmm10
+    ; 3rd filter of 4
+    vmovdqu32 zmm12 {k1}{z}, [rdi]              ; from layer 1
+    vmovdqu32 zmm13 {k1}{z}, [rdi + r12]        ; from layer 2
+    vmovdqu32 zmm14 {k1}{z}, [rdi + r12*2]      ; from layer 3
+    vfmadd231ps zmm11, zmm1, zmm12              ; zmm11 += zmm1 * zmm12
+    vfmadd231ps zmm11, zmm2, zmm13              ; zmm11 += zmm2 * zmm13
+    vfmadd231ps zmm11, zmm3, zmm14              ; zmm11 += zmm3 * zmm14
+    ; 4th filter of 4
+    vmovdqu32 zmm16 {k1}{z}, [rdi]              ; from layer 1
+    vmovdqu32 zmm17 {k1}{z}, [rdi + r12]        ; from layer 2
+    vmovdqu32 zmm18 {k1}{z}, [rdi + r12*2]      ; from layer 3
+    vfmadd231ps zmm15, zmm1, zmm16              ; zmm15 += zmm1 * zmm16
+    vfmadd231ps zmm15, zmm2, zmm17              ; zmm15 += zmm2 * zmm17
+    vfmadd231ps zmm15, zmm3, zmm18              ; zmm15 += zmm3 * zmm18
 
-    xor r8, r8      ; sample index in batch
-    xor r9, r9      ; i for input row
-    xor r10, r10    ; j for input column
-.kernel_loop:
-    vmovdqu32 zmm1 {k1}{z}, [rsi]               ; first layer of the 3d window(3*3) of input
-    vfmadd231ps zmm0, zmm1, zmm2
+    inc 
+
+
     
-    vmovdqu32 zmm1 {k1}{z}, [rsi + rbx]         ; second ... ...
-    vfmadd231ps zmm0, zmm1, zmm3
-
-    vmovdqu32 zmm1 {k1}{z}, [rsi + rbx*2]       ; third ... ... 
-    vfmadd231ps zmm0, zmm1, zmm4
-
-    ; sum of 16 float in zmm0
-    vextractf32x8 ymm1, zmm0, 1   ; high 256 bits
-    vaddps  ymm0, ymm0, ymm1      ; add high half to low half
-    vextractf128 xmm1, ymm0, 1    ; high 128 bits
-    vaddps  xmm0, xmm0, xmm1      ; add high to low        ---  xmm0 [a, b, c, d]
-    haddps  xmm0, xmm0            ; adds pairs of floats inside  --- [a+b, c+d, ?, ?]
-    haddps  xmm0, xmm0            ; then                         --- [sum_total, ?, ?, ?]
-    ; zmm0 = [f0, f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11, f12, f13, f14, f15]
-    ; ymm0 = [f0+f8, f1+f9, f2+f10, f3+f11, f4+f12, f5+f13, f6+f14, f7+f15]
-    ; xmm0 = [f0+f8+f4+f12, f1+f9+f5+f13, f2+f10+f6+f14, f3+f11+f7+f15]
-    ; xmm0 = sum of f1...f15
-
-    movss xmm1, [conv1_b + rcx*4] ; bias
-    addss xmm0, xmm1
-
-    movss [conv1_out ], xmm0
-
-    add rsi, 3*4    ; move input  cursor
-    add rdx, 32*4   ; move output cursor
-    
-    inc r10
-    cmp r10, 128
-    jl .next
-    xor r10, r10
-    add rsi, 2*3*4     ; next row 2*3*4 ,skip 2 last pixel
-
-    inc r9
-    cmp r9, 128
-    jl .next
-    xor r9, r9
-    add rsi, 2*130*4    ; next sample 2*130*4 , skip 2 last row
-    add rdx, 31*128*128*4  ; skip 31 filter output for reaching same filter in next sample output
-
-    inc r8
-    cmp r8, B
-    jl .next
-    xor r8, r8
-    lea rdx, [rel input]    ; go back
-    add rdx, 128*128*4      ; next filter output
-
-    cmp rcx, 32
-    jl .filter_loop
-
-    ret                 ; end
-
-.next:
-    jmp .kernel_loop
