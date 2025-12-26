e662a76961bc5cb5b48614b85e037d26000973e4
mohammad-ghaderi <mohammadunique2002@gmail.com>
2025-12-26 20:52:03 +0330

    Remove stupid mask

--- update_weights.asm
+++ update_weights.asm
@@ -16,9 +16,7 @@ update_weights:
     ; update fc2_w
     lea rdi, [rel d_fc2_w]
     lea rsi, [rel fc2_w]
-    mov rcx, 128
-    mov rbx, 0xFFFF
-    kmovw k1, ebx               ; k1 = mask
+    mov rcx, 128 
     call add_to_vector
 
     ; update fc2_b
@@ -37,66 +35,50 @@ update_weights:
     ; update fc1_w
     lea rdi, [rel d_fc1_w]
     lea rsi, [rel fc1_w]
-    mov rcx, 128*16*16*128
-    mov rbx, 0xFFFF
-    kmovw k1, ebx               
+    mov rcx, 128*16*16*128           
     call add_to_vector
 
     ; update fc1_b
     lea rdi, [rel d_fc1_b]
     lea rsi, [rel fc1_b]
-    mov rcx, 128
-    mov rbx, 0xFFFF
-    kmovw k1, ebx               
+    mov rcx, 128           
     call add_to_vector
 
     ; update conv3_w
     lea rdi, [rel d_conv3_w]
     lea rsi, [rel conv3_w]
-    mov rcx, 3*3*64*128
-    mov rbx, 0xFFFF
-    kmovw k1, ebx               
+    mov rcx, 3*3*64*128           
     call add_to_vector
 
     ; update conv3_b
     lea rdi, [rel d_conv3_b]
     lea rsi, [rel conv3_b]
-    mov rcx, 128
-    mov rbx, 0xFFFF
-    kmovw k1, ebx               
+    mov rcx, 128           
     call add_to_vector
 
     ; update conv2_w
     lea rdi, [rel d_conv2_w]
     lea rsi, [rel conv2_w]
-    mov rcx, 3*3*32*64
-    mov rbx, 0xFFFF
-    kmovw k1, ebx               
+    mov rcx, 3*3*32*64           
     call add_to_vector
 
     ; update conv2_b
     lea rdi, [rel d_conv2_b]
     lea rsi, [rel conv2_b]
-    mov rcx, 64
-    mov rbx, 0xFFFF
-    kmovw k1, ebx               
+    mov rcx, 64           
     call add_to_vector
 
     
     ; update conv1_w
     lea rdi, [rel d_conv1_w]
     lea rsi, [rel conv1_w]
-    mov rcx, 3*3*3*32
-    mov rbx, 0000000111111111b
-    kmovw k1, ebx               
+    mov rcx, 3*3*3*32           
     call add_to_vector
 
     ; update conv1_b
     lea rdi, [rel d_conv1_b]
     lea rsi, [rel conv1_b]
-    mov rcx, 32
-    mov rbx, 0xFFFF
-    kmovw k1, ebx               
+    mov rcx, 32           
     call add_to_vector
 
     ret
@@ -106,27 +88,26 @@ update_weights:
 ; rdi = input
 ; rsi = output
 ; rcx = size
-; k1 = mask
-; output += input
+; output -= learning_rate*input
 add_to_vector:
     shr rcx, 4
 .loop:
-    vmovups zmm1{k1}{z}, [rdi]
+    vmovups zmm1, [rdi]
     vbroadcastss zmm2, [Learning_rate]
     vmulps zmm1, zmm1, zmm2
 
     vbroadcastss zmm2, [BATCH_SIZE_INV]
     vmulps zmm1, zmm1, zmm2
 
-    vmovups zmm0{k1}{z}, [rsi]
-    vsubps  zmm0{k1}, zmm0, zmm1
+    vmovups zmm0, [rsi]
+    vsubps  zmm0, zmm0, zmm1
 
 
-    vmovups [rsi]{k1}, zmm0
+    vmovups [rsi], zmm0
 
     ; fill the vector with zero
     vpxorq zmm1, zmm1, zmm1
-    vmovups [rdi]{k1}, zmm1
+    vmovups [rdi], zmm1
     add rsi, 64
     add rdi, 64
 
